debf31301220fc998ad2bfae49242882be080d70
Nadav-Really-Virtual <110404994+Nadav-Really-Virtual@users.noreply.github.com>
2026-06-15 23:34:48 +0300

    more audio fixes

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ const popChatIntro = new Howl({
     //volume: 0.4 // Kept slightly quieter in background
 });
 //
-const buttonSound = new Howl({ src: ['button-8'], html5: true });
-const sendSound = new Howl({ src: ['message-sent'], html5: true });
-const errorSound = new Howl({ src: ['error_01'], html5: true });
+const buttonSound = new Howl({ src: ['button-8.wav'], html5: true });
+const sendSound = new Howl({ src: ['message-sent.wav'], html5: true });
+const errorSound = new Howl({ src: ['./audio/error_01.wav'], html5: true });
 
 // 2. Core Navigation Engine
 function navigateTo(screenId) {
